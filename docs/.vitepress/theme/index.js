ee0618933100b3acdd4658a365082d5faf5b06f0
anibalinbalin <anibalin@gmail.com>
2024-08-20 17:49:12 -0300

    saque los insigts de nuevo

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,5 @@
 import DefaultTheme from "vitepress/theme";
 import { injectSpeedInsights } from "@vercel/speed-insights";
-import { inject as injectAnalytics } from "@vercel/analytics";
 
 export default {
   ...DefaultTheme,
@@ -8,8 +7,6 @@ export default {
   enhanceApp({ app, router, siteData }) {
     if (typeof window !== "undefined") {
       // Call the Vercel Speed Insights and Analytics injection functions
-      injectSpeedInsights();
-      injectAnalytics();
     }
   },
 };
